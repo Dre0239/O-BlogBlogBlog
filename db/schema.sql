4fd7676786ae9fc1c8ea31b48757cd632f01c10c
Dredog0239 <almays29@gmail.com>
2023-05-16 18:04:29 -0700

    updating files and folders.

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
 /* Will hold data and pass it on to post, get, etc*/
-DROP DATABASE IF EXISTS O-blog_db;
+DROP DATABASE IF EXISTS tech_blog_db;
 
-CREATE DATABASE O-blog_db;
+CREATE DATABASE tech_blog_db;
