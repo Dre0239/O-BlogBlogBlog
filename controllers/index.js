7507acad642e9e90b069d776da448bf01a97dfb9
Dredog0239 <almays29@gmail.com>
2023-05-16 17:15:33 -0700

    updating files and folders.

--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,17 @@
-// collects the packaged api routes
-const router = require("express").Router();
-
-// this is the packaged api routes index file
-const apiRoutes = require("./api");
-
-
+const router = require('express').Router();
+const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 
-router.use("/api", apiRoutes);
-
+router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
 
 
 router.use((req, res) => {
-  res.status(404).end();
+    res.status(404).end();
 });
+
 
 module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
 /* Will hold data and pass it on to post, get, etc*/
-DROP DATABASE IF EXISTS jtn_db;
+DROP DATABASE IF EXISTS O-blog_db;
 
-CREATE DATABASE jtn_db;
+CREATE DATABASE O-blog_db;
